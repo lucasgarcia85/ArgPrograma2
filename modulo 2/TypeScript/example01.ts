3b1991699622ced289965001a9f4868eda1d34df
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2021-12-09 10:06:13 -0300

    incluido typescript

---
+++ modulo 2/TypeScript/example01.ts
@@ -0,0 +1,9 @@
+let a: number;
+let b: string;
+let c = 101;
+
+   // c ="one"; error porque es string originalmente
+let recursos: (string | number) [];
+    recursos = ["memoria", "disco", 100];
+
+ let recursos2:string[] = ["memoria","disco","procesador"];
