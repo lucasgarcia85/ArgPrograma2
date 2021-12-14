8665a7d56841cb63a87b27b2458ebb54d36f1343
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2021-12-14 08:37:59 -0300

    modulo 2

---
+++ modulo 2/TypeScript/output/app.js
@@ -0,0 +1,10 @@
+"use strict";
+let message = "hola mundo";
+console.log(message);
+let a;
+let b;
+let c = 101;
+// c ="one"; error porque es string originalmente
+let recursos;
+recursos = ["memoria", "disco", 100];
+let recursos2 = ["memoria", "disco", "procesador"];
